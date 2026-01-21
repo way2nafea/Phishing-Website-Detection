b88a1652d1c1173fed9d963ce693298df6d70d11
way2nafea <way2abdulnafea@gmail.com>
2026-01-21 19:08:22 +0530

    Fix Render port issue

--- app.py
+++ app.py
@@ -41,6 +41,8 @@ def index():
     # GET request
     return render_template("index.html")
 
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
